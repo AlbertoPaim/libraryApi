82c8bba7577cd31d71dbd71def74612b75a1f97f
Alberto Paim <albertoopaim@gmail.com>
2024-12-09 12:00:48 -0300

    library api first commit

---
+++ libraryApi/src/test/java/com/albertopaim/libraryApi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertopaim.libraryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
