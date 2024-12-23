759a2fab06883e2bda1ed1b597cb7040d568a4fc
Alberto Paim <albertoopaim@gmail.com>
2024-12-23 09:59:11 -0300

    feat: autorController

---
+++ libraryApi/src/main/java/com/albertopaim/libraryApi/Controller/AutorController.java
@@ -0,0 +1,11 @@
+package com.albertopaim.libraryApi.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autores")
+public class AutorController {
+
+
+}
