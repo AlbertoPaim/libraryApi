2e4b8b5d8fd85c2555aab853f913f907657c4537
Alberto Paim <albertoopaim@gmail.com>
2025-06-29 15:36:05 -0300

    feat: create livro controller

--- libraryApi/src/main/java/com/albertopaim/libraryApi/Controller/dto/CadastroLivroDTO.java
+++ libraryApi/src/main/java/com/albertopaim/libraryApi/Controller/dto/CadastroLivroDTO.java
@@ -1,16 +1,25 @@
 package com.albertopaim.libraryApi.Controller.dto;
 
 import com.albertopaim.libraryApi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record CadastroLivroDTO(String isbn,
-                               String titulo,
-                               LocalDate dataPublicacao,
-                               GeneroLivro genero,
-                               BigDecimal preco,
-                               UUID idAutor
-                               ) {
+public record CadastroLivroDTO(
+        @NotBlank(message = "Campo obrigatório")
+        String isbn,
+        @NotBlank(message = "Campo obrigatório")
+        String titulo,
+        @NotNull(message = "Campo obrigatório")
+        @Past(message = "Não pode ser data passada")
+        LocalDate dataPublicacao,
+        GeneroLivro genero,
+        BigDecimal preco,
+        @NotNull(message = "Campo obrigatório")
+        UUID autor
+) {
 }
--- libraryApi/src/main/java/com/albertopaim/libraryApi/Controller/dto/ResultadoPesquisaLivroDTO.java
+++ libraryApi/src/main/java/com/albertopaim/libraryApi/Controller/dto/ResultadoPesquisaLivroDTO.java
@@ -5,10 +5,11 @@ import com.albertopaim.libraryApi.model.GeneroLivro;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record ResultadoPesquisaLivroDTO(String isbn,
-                                        String titulo,
-                                        LocalDate dataPublicacao,
-                                        GeneroLivro genero,
-                                        BigDecimal preco,
-                                        AutorDTO autor) {
+public record ResultadoPesquisaLivroDTO(
+        String isbn,
+        String titulo,
+        LocalDate dataPublicacao,
+        GeneroLivro genero,
+        BigDecimal preco,
+        AutorDTO autor) {
 }
